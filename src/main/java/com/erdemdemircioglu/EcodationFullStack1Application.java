d1cc36f7c928813221c94fcac47f1a7579395997
Erdem6161 <erdem.demircioglu@gmail.com>
2022-03-05 10:31:12 +0300

    Initial commit

---
+++ src/main/java/com/erdemdemircioglu/EcodationFullStack1Application.java
@@ -0,0 +1,13 @@
+package com.erdemdemircioglu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcodationFullStack1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcodationFullStack1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/erdemdemircioglu/EcodationFullStack1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdemdemircioglu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcodationFullStack1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
